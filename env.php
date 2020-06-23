dc5037661c456eff1beb8f444594ea93981b0f6a
cuongnq <cuongng1296@gmail.com>
2020-06-23 16:24:35 +0700

    push

--- env.php
+++ env.php
@@ -2,7 +2,7 @@
 define("Client_ID", '3f70ac1e64a6c7d172dd');
 define("Client_Secret", 'd597d1e6ddd5441c1fa36df5319df93829dc2d01');
 define("ServerName", 'localhost');
-define("DB", 'kkcdm');
+define("DB", 'github');
 define("UserName", 'root');
 define("Password", '');
 ?>
